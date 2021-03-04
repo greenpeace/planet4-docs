639c0818c8a31199f1abb1c9042bcfab5b99430d
Nikos Roussos <nikos@roussos.cc>
2021-03-04 13:46:54 +0200

    Remove docker-compose submodule

--- sync-repos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git submodule update --remote --merge
-
-yes | cp repos/planet4-docker-compose/docs/installation.md docs/development/installation.md
-yes | cp repos/planet4-docker-compose/docs/testing.md docs/ci-cd/testing/acceptance-tests.md
